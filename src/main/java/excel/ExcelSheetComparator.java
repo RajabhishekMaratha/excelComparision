ef219a9b8f8770cebbe058f4e80f25275d46cabf
Rajabhishek <rajabhishekmaratha@gmail.com>
2025-02-26 10:01:11 +0530

    Minor updates

--- src/main/java/excel/ExcelSheetComparator.java
+++ src/main/java/excel/ExcelSheetComparator.java
@@ -26,96 +26,27 @@ public class ExcelSheetComparator {
             Sheet sheet2 = workbook.getSheet(sheet2Name);
 
             int lastRow1 = sheet1.getLastRowNum();
-            int lastCol1 = sheet1.getRow(0).getLastCellNum();
             int lastRow2 = sheet2.getLastRowNum();
-            int lastCol2 = sheet2.getRow(0).getLastCellNum();
 
-            // Column Mapping
-            int[] colMap = new int[lastCol1];
-            for (int j = 0; j < lastCol1; j++) {
-                String header1 = "";
-                if (sheet1.getRow(0).getCell(j) != null) {
-                    header1 = sheet1.getRow(0).getCell(j).getStringCellValue().replaceAll(" ", "");
-                }
-                colMap[j] = -1;
-                for (int k = 0; k < lastCol2; k++) {
-                    String header2 = "";
-                    if (sheet2.getRow(0).getCell(k) != null) {
-                        header2 = sheet2.getRow(0).getCell(k).getStringCellValue().replaceAll(" ", "");
-                    }
-                    if (header1.equals(header2)) {
-                        colMap[j] = k;
-                        break;
-                    }
-                }
-            }
+            // *** COLUMN MAPPING SECTION ***
+            // Create header maps for both sheets (header name -> column index)
+            Map<String, Integer> sheet1HeaderMap = getHeaderMap(sheet1);
+            Map<String, Integer> sheet2HeaderMap = getHeaderMap(sheet2);
+            // *** END OF COLUMN MAPPING SECTION ***
 
-            // Row Mapping
-            Map<String, Integer> sheet1IdMap = new HashMap<>();
-            Map<String, Integer> sheet2IdMap = new HashMap<>();
+            // *** ROW MAPPING SECTION ***
+            // Create ID maps for both sheets (ID -> row index)
+            Map<String, Integer> sheet1IdMap = getIdMap(sheet1);
+            Map<String, Integer> sheet2IdMap = getIdMap(sheet2);
+            // *** END OF ROW MAPPING SECTION ***
 
-            for (int i = 1; i <= lastRow1; i++) {
-                String id1 = getCellValueAsString(sheet1.getRow(i).getCell(0)).replaceAll(" ", "");
-                sheet1IdMap.put(id1, i);
-            }
-
-            for (int i = 1; i <= lastRow2; i++) {
-                String id2 = getCellValueAsString(sheet2.getRow(i).getCell(0)).replaceAll(" ", "");
-                sheet2IdMap.put(id2, i);
-            }
-
-            // Compare Rows and Columns
             for (Map.Entry<String, Integer> entry : sheet1IdMap.entrySet()) {
                 String id1 = entry.getKey();
                 int row1 = entry.getValue();
 
                 if (sheet2IdMap.containsKey(id1)) {
                     int row2 = sheet2IdMap.get(id1);
-
-                    for (int j = 0; j < lastCol1; j++) {
-                        if (colMap[j] != -1) {
-                            int k = colMap[j];
-                            String val1 = getCellValueAsString(sheet1.getRow(row1).getCell(j)).replaceAll(" ", "");
-                            String val2 = getCellValueAsString(sheet2.getRow(row2).getCell(k)).replaceAll(" ", "");
-
-                            CellStyle greenStyle = workbook.createCellStyle();
-                            greenStyle.setFillForegroundColor(IndexedColors.GREEN.getIndex());
-                            greenStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-
-                            CellStyle redStyle = workbook.createCellStyle();
-                            redStyle.setFillForegroundColor(IndexedColors.RED.getIndex());
-                            redStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-
-                            if ((val1.equals("-") && val2.isEmpty()) || (val1.isEmpty() && val2.equals("-")) || val1.equals(val2)) {
-                                if (sheet1.getRow(row1).getCell(j) == null) {
-                                    sheet1.getRow(row1).createCell(j).setCellStyle(greenStyle);
-                                } else {
-                                    sheet1.getRow(row1).getCell(j).setCellStyle(greenStyle);
-                                }
-
-                                if (sheet2.getRow(row2).getCell(k) == null) {
-                                    sheet2.getRow(row2).createCell(k).setCellStyle(greenStyle);
-                                } else {
-                                    sheet2.getRow(row2).getCell(k).setCellStyle(greenStyle);
-                                }
-
-                            } else {
-                                if (sheet1.getRow(row1).getCell(j) == null) {
-                                    sheet1.getRow(row1).createCell(j).setCellStyle(redStyle);
-                                } else {
-                                    sheet1.getRow(row1).getCell(j).setCellStyle(redStyle);
-                                }
-
-                                if (sheet2.getRow(row2).getCell(k) == null) {
-                                    sheet2.getRow(row2).createCell(k).setCellStyle(redStyle);
-                                } else {
-                                    sheet2.getRow(row2).getCell(k).setCellStyle(redStyle);
-                                }
-                            }
-                        }
-                    }
-                } else {
-                    System.out.println("ID: " + id1 + " not found in Sheet2");
+                    compareRow(sheet1, sheet2, row1, row2, sheet1HeaderMap, sheet2HeaderMap, workbook);
                 }
             }
 
@@ -128,6 +59,77 @@ public class ExcelSheetComparator {
         }
     }
 
+    // Method to create a header map (header name -> column index) for a sheet
+    private static Map<String, Integer> getHeaderMap(Sheet sheet) {
+        Map<String, Integer> headerMap = new HashMap<>();
+        Row headerRow = sheet.getRow(0);
+        if (headerRow != null) {
+            for (int i = 0; i < headerRow.getLastCellNum(); i++) {
+                Cell cell = headerRow.getCell(i);
+                if (cell != null && cell.getCellType() == CellType.STRING) {
+                    headerMap.put(cell.getStringCellValue().replaceAll(" ", ""), i);
+                }
+            }
+        }
+        return headerMap;
+    }
+
+    // Method to create an ID map (ID -> row index) for a sheet
+    private static Map<String, Integer> getIdMap(Sheet sheet) {
+        Map<String, Integer> idMap = new HashMap<>();
+        for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+            Cell idCell = sheet.getRow(i).getCell(0);
+            String id = getCellValueAsString(idCell).replaceAll(" ", "");
+            if (!id.isEmpty()) {
+                idMap.put(id, i);
+            }
+        }
+        return idMap;
+    }
+
+    // Method to compare two rows based on header mappings and apply cell styles
+    private static void compareRow(Sheet sheet1, Sheet sheet2, int row1, int row2, Map<String, Integer> sheet1HeaderMap, Map<String, Integer> sheet2HeaderMap, Workbook workbook) {
+        CellStyle greenStyle = workbook.createCellStyle();
+        greenStyle.setFillForegroundColor(IndexedColors.GREEN.getIndex());
+        greenStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+        CellStyle redStyle = workbook.createCellStyle();
+        redStyle.setFillForegroundColor(IndexedColors.RED.getIndex());
+        redStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+        for (Map.Entry<String, Integer> headerEntry : sheet1HeaderMap.entrySet()) {
+            String header = headerEntry.getKey();
+            int col1 = headerEntry.getValue();
+
+            if (sheet2HeaderMap.containsKey(header)) {
+                int col2 = sheet2HeaderMap.get(header);
+
+                String val1 = getCellValueAsString(sheet1.getRow(row1).getCell(col1)).replaceAll(" ", "");
+                String val2 = getCellValueAsString(sheet2.getRow(row2).getCell(col2)).replaceAll(" ", "");
+
+                if ((val1.equals("-") && val2.isEmpty()) || (val1.isEmpty() && val2.equals("-")) || val1.equals(val2)) {
+                    setCellStyle(sheet1, row1, col1, greenStyle);
+                    setCellStyle(sheet2, row2, col2, greenStyle);
+                } else {
+                    setCellStyle(sheet1, row1, col1, redStyle);
+                    setCellStyle(sheet2, row2, col2, redStyle);
+                }
+            }
+        }
+    }
+
+    private static void setCellStyle(Sheet sheet, int row, int col, CellStyle style) {
+        Row r = sheet.getRow(row);
+        if(r == null){
+            r = sheet.createRow(row);
+        }
+        Cell c = r.getCell(col);
+        if(c == null){
+            c = r.createCell(col);
+        }
+        c.setCellStyle(style);
+    }
+
     private static String getCellValueAsString(Cell cell) {
         if (cell == null) {
             return "";
@@ -138,7 +140,7 @@ public class ExcelSheetComparator {
                     return cell.getStringCellValue();
                 case NUMERIC:
                     return String.valueOf((int) cell.getNumericCellValue());
-                case BOOLEAN:
+                case BOOLEAN:  
                     return String.valueOf(cell.getBooleanCellValue());
                 case FORMULA:
                     return cell.getCellFormula();
@@ -146,7 +148,6 @@ public class ExcelSheetComparator {
                     return "";
             }
         } catch (Exception e) {
-            e.printStackTrace();
             return "";
         }
     }
